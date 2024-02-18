5b232b629e95c076febcdc5bf90425e525a744c4
Benson Tran <135925601+BensonTran12@users.noreply.github.com>
2024-02-18 00:08:49 -0800

    Update algo.js

--- components/broncobackend/algo.js
+++ components/broncobackend/algo.js
@@ -203,3 +203,14 @@ printAll();
 module.exports = {
   displayData,
 };
+
+fetch()
+  .then(response=> {
+
+    if (!response.ok){
+      throw new Error("Could not fetch resource");
+    }
+    return response.json();
+  })
+  .then(data =>console.log(data.(insertdatatyoe)))
+  .catch(error=>console.log.(error));
