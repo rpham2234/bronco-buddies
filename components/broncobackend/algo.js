199d71b20f0a803e4dfaea50defb1474c5c28050
Hobbesters <oumsters616@gmail.com>
2024-02-17 21:41:14 -0800

    Update algo.js
    Fixed most bugs in algo.js lmao

--- components/broncobackend/algo.js
+++ components/broncobackend/algo.js
@@ -1,9 +1,8 @@
 //main function
 //read from user funciton
-
-var listOfTasks = []; // stores our objects/tasks in a list
+var listOfTasks = new Array(6).fill(null); // stores our objects/tasks in a list
 var userAvailiableTimes = new Array(7).fill().map((i) => new Array(20).fill(0)); //2d array to have each row from monday to sunday
-var schedule = new Array(7).fill().map((i) => new Array(20).fill("Busy")); // stores objects
+var schedule = new Array(7).fill().map((i) => new Array(20).fill(0)); // stores objects
 var today = 1; //single value for calc
 
 // Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday  = 6, Sunday = 7
@@ -16,6 +15,7 @@ class Task {
     this.dueDate = dueDate;
     this.assignmentType = assignmentType;
     this.timeNeeded = timeNeeded;
+    this.importance;
   }
 }
 
@@ -66,21 +66,34 @@ function addTasksToDays() {
   let counter; //time in a task remaining to be added during the adding process of that task
   let timeRemainingInDay = 0; //time left within a day
   let numOfTaskInDay = 0;
+  let addedOnceFlag = 0;
+
+  let numOfAllTasks = 0;
+  while(listOfTasks[numOfAllTasks] != null){
+    numOfAllTasks++;
+  }
 
   for (let i = 0; i < 7; i++) {
     //counting time in day
     timeRemainingInDay = 0;
     numOfTaskInDay = 0;
+    //console.log("Day: ", i);
+    //console.log("Tasks Left: ", numOfAllTasks, "Current Task: ", objectPointer);
+    if (numOfAllTasks == 0 && addedOnceFlag == 1){
+      //console.log("breaking");
+      break;
+    }
     for (let j = 0; j < 20; j++) {
       //counting times in hours within days
-      console.log("userAvailiableTimes: ", userAvailiableTimes[i][j]);
+      //console.log("userAvailiableTimes: ", userAvailiableTimes[i][j]);
       if (userAvailiableTimes[i][j] === 1) {
         timeRemainingInDay++;
-        console.log("TimeAvailibleIs==", timeRemainingInDay);
+        //console.log("TimeAvailibleIs==", timeRemainingInDay);
       }
     }
     counter = listOfTasks[objectPointer].timeNeeded;
     for (let k = 0; k < 20; k++) {
+      //console.log("Tasks Left: ", numOfAllTasks, "Current Task: ", objectPointer);
       if (timeRemainingInDay === 0) {
         //console.log("timeRemainingInDay==0");
         break;
@@ -89,14 +102,24 @@ function addTasksToDays() {
         //console.log("timeRemainingInDay>counter");
         break;
       }
+      if (numOfAllTasks == 0){
+        break;
+      }
       if (counter === 0) {
+        //console.log("Added Task Completely");
         objectPointer++;
         numOfTaskInDay++;
+        numOfAllTasks--;
+        if (numOfAllTasks == 0){
+          break;
+        }
         counter = listOfTasks[objectPointer].timeNeeded;
+        addedOnceFlag = 1;
       }
       if (numOfTaskInDay >= 4){
         break;
       }
+      
       if (userAvailiableTimes[i][k] === 1) {
         //
         //console.log("userAvail==1", objectPointer, listOfTasks[objectPointer]);
@@ -114,17 +137,17 @@ function priority() {
   //nested for loop
   //bubble and sort to find highest priority i.e largest number to smallest
   for (let i = 0; i < 7; i++) {
-    for (let j = 0; j < 20 - i; j++) {
-      if (arr[j] < arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+    for (let j = 0; j < 20 - 1 - i; j++) {
+      if (listOfTasks[j].importance < listOfTasks[j + 1].importance) {
+        [listOfTasks[j], listOfTasks[j + 1]] = [listOfTasks[j + 1], listOfTasks[j]];
+        console.log("sorting...");
       }
     }
   }
   return;
 }
-
 function displayData() {
-  return schedule;
+  
   
 }
 
@@ -146,12 +169,35 @@ readFromUser("TTQuiz", "Sunday", 2, 4, 2);
 readFromUser("ECEN20Essay", "Thursday", 3, 3, 3);
 readFromUser("StudyJapanese", "Sunday", 1, 5, 4);
 readFromUser("ECEN21Quiz", "Tuesday", 2, 1, 5);
-determineUserFreeTime(1, 2);
+determineUserFreeTime(1, 1);
 determineUserFreeTime(1, 3);
 determineUserFreeTime(1, 4);
-determineUserFreeTime(1, 10);
+determineUserFreeTime(1, 5);
+determineUserFreeTime(1, 8);
 determineUserFreeTime(1, 12);
+determineUserFreeTime(1, 13);
+determineUserFreeTime(1, 15);
+determineUserFreeTime(1, 16);
+determineUserFreeTime(2, 1);
+determineUserFreeTime(2, 3);
+determineUserFreeTime(2, 4);
+determineUserFreeTime(2, 5);
+determineUserFreeTime(2, 8);
+determineUserFreeTime(2, 12);
+determineUserFreeTime(2, 13);
+determineUserFreeTime(2, 15);
+determineUserFreeTime(2, 16);
+determineUserFreeTime(4, 1);
+determineUserFreeTime(4, 3);
+determineUserFreeTime(4, 4);
+determineUserFreeTime(4, 5);
+determineUserFreeTime(4, 8);
+determineUserFreeTime(4, 12);
+determineUserFreeTime(4, 13);
+determineUserFreeTime(4, 15);
+determineUserFreeTime(4, 16);
 addTasksToDays();
+//priority();
 printAll();
 
 module.exports = {
